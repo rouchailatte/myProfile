2b5707cb6df80637e3bb730a9048f8365424df8e
rouchailatte <byyagp@gmail.com>
2014-10-24 23:38:24 -0700

    fancybox added, and refractor the navbar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,18 +3,22 @@ class WelcomeController < ApplicationController
   end
 
   def edu
+    @cog_str = "edu"
   end
 
   def exp
+    @cog_str = "exp"
   end
 
   def proj
+    @cog_str = "proj"
   end
 
   def img
+    @cog_str = "img"
   end
 
   def skill
-    
+    @cog_str = "skill"
   end
 end
